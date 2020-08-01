dcf069594dbb0954f15a8712d824a7a09b8d75f7
Benjamin Fuhlbrügge <s176110@hft-leipzig.de>
2020-08-01 13:31:14 +0200

    transfer encoding

--- http_asym.py
+++ http_asym.py
@@ -79,7 +79,7 @@ sniff(filter = "tcp port " + str(http_port), prn=get_http_packet, count = 1)
 
 #ls(http_answer)
 print(http_answer.getlayer(IP).src)
-print(http_answer.getlayer(http).Status-Line)
+print(http_answer.getlayer(HTTP).Transfer-Encoding)
 
 
 
