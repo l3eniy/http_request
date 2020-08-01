9597f3b0f106a494dc8ed021b07e100582259c25
Benjamin Fuhlbrügge <s176110@hft-leipzig.de>
2020-08-01 20:34:25 +0200

    test

--- http_asym.py
+++ http_asym.py
@@ -115,7 +115,7 @@ def get_http_packet(packet):
             print("")
 
 
-            print(packet.getlayer(HTTPResponse)[0:10])
+            print(packet.getlayer(HTTPResponse)[0:20])
 
 
             print("############## Body ###################")
