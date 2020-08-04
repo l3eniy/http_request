20cc2ca857918879868a1c151842174cfcb7e17f
Benjamin Fuhlbrügge <s176110@hft-leipzig.de>
2020-08-04 14:01:22 +0200

    wwd

--- http_asym.py
+++ http_asym.py
@@ -99,7 +99,7 @@ def start_TCP_IN_Thread(packet):
     t.start()
 
 def sniff_all_packets():
-    sniff(session=TCPSession, filter = "tcp port " + str(http_port), prn=start_TCP_IN_Thread(), store=False, count = 5)
+    sniff(session=TCPSession, filter = "tcp port " + str(http_port), prn=start_TCP_IN_Thread, store=False, count = 5)
     return
 
 
