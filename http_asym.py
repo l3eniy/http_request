0f566caf6fe4518778babdd412d9bc57524ca863
Benjamin Fuhlbrügge <s176110@hft-leipzig.de>
2020-08-04 17:54:46 +0200

    wdw

--- http_asym.py
+++ http_asym.py
@@ -20,7 +20,6 @@ import os
 
 
 ### Variabeln definieren
-
 vtep_dst = "192.168.10.149"
 vtep_src = "172.20.10.254" #Hier muss eine IP aus meinem Subnetz stehen, sonst verwirft der erste Hop das Paket!
 vxlanport = 4789
@@ -42,37 +41,15 @@ URG = 0x20
 ECE = 0x40
 CWR = 0x80
 
-
 ### Check if debug is enabled
 if len(sys.argv) > 3:
     debug = 1
 else:
     debug = 0
 
-
 ### VXLAN Paket: Hierueber werden Ethernet Frames ins LAN eingefuert
 VXLAN = IP(src=vtep_src,dst=vtep_dst)/UDP(sport=vxlanport,dport=vxlanport)/VXLAN(vni=vx_vnid,flags="Instance")/Ether(dst=mac_dst,src=mac_src)
-
-### syn_ack_do ist die Funktion, die beim sniffen des SYN/ACK Pakets ausgefuehrt wird
-# Fuer das folgenden ACK Paket sind folende Parameter wichtig: Dst_Port, ACK#, SEQ#
-def syn_ack_do(packet):
-    #print(packet.summary())
-    #print(packet[TCP].dport)
-    global syn_ack_dport
-    syn_ack_dport = packet[TCP].dport
-    global syn_ack_ack
-    syn_ack_ack = packet[TCP].ack
-    global syn_ack_seq
-    syn_ack_seq = packet[TCP].seq
-    if debug:
-        print("############## SYN/ACK packet received ##############")
-        print("dport = " + str(syn_ack_dport))
-        print("ACK# = " + str(syn_ack_ack))
-        print("SEQ# = " + str(syn_ack_seq))
-        print("")
-    return
-
-
+### getStr ist der String im HTTP Request
 getStr = 'GET / HTTP/1.1\r\nHost:' + dest + '\r\nAccept-Encoding: 8bit\r\n\r\n'
 
 #$$$$$ SEND TCP Package
@@ -96,32 +73,27 @@ def syn_ack_received_send_http_req(src_port, seqnr, acknr):
 
 
 
-### NEUE Sniff Funktion fuer sniff_http_response_thread
+### Sniff Funktion fuer sniff_http_response_thread
 http_content = ""
 def get_http_packet1(packet):
-    #print("\r\n\r\n\r\n######### Paket ist eingetroffen! #########")
-    #print(packet.summary())
     if packet.haslayer(HTTPResponse) is True:
         global http_status
+        ### http status ist die Response Status Nachricht. zB HTTP1.1/200/OK
         http_status = str(packet.getlayer(HTTPResponse).Http_Version) + " " + str(packet.getlayer(HTTPResponse).Status_Code) + " " + str(packet.getlayer(HTTPResponse).Reason_Phrase) 
-        #print("\r\nPacket has layer HTTPResponse")
-        #ls(packet.getlayer(HTTPResponse))
-    #if packet.haslayer(HTTP) is True:
-    #    print("\r\nPacket has layer HTTP")
-    #    ls(packet.getlayer(HTTP))
     if packet.haslayer(Raw) is True:
         global http_content
+        ### http content sind header und body und wird ggf aus mehreren Paketen zusammengesetzt. Nur die Layer Raw besitzt Teile von HTTP Content
         http_content += str(packet.getlayer(Raw).load)
-        #print("\r\nPacket has layer Raw")
-        #print("\r\n" + packet.getlayer(Raw).load)
+
 
 
 
 ### Sniff Funktion um HTTPResponse zu finden
 # Sie filtert auf TCP Pakete mit der ACK Nummer 58. Der Request hast eine Laenge von 57. Die Ack Nummer ist Length + 1
 def sniff_http_response_thread():
-    #sniff(filter = "tcp port " + str(http_port) + " and tcp[11] == 58 and tcp[13] == 24 and greater 100", prn=get_http_packet, count = 1)  # + " and tcp[tcpflags] & tcp-ack == 58"
     sniff(session=TCPSession, filter = "tcp port 80", prn=get_http_packet1, store=False, count = 5)
+
+
     print(http_status)
     print("")
     print(http_content)
@@ -145,38 +117,44 @@ def worker(packet):
     in_seq = packet[TCP].seq
     in_ack = packet[TCP].ack
     dst_port = packet.getlayer(TCP).dport
-    print ("IP Source:          " + str(packet.getlayer(IP).src) + ":" + str(packet.getlayer(TCP).sport))
-    print ("IP Destin:          " + str(packet.getlayer(IP).dst) + ":" + str(packet.getlayer(TCP).dport))
-    print("TCP Payload Length:  " + str(payload_length))
-    print("Flags:               " + str(flags))
-    print("TCP ACK#:            " + str(in_ack))
-    print("TCP SEQ#:            " + str(in_seq))
-    if flags & PSH:
-        print "PSH Flag set"
-    if (flags & (SYN ^ ACK)) == 18:
-        print "SYN/ACK --> both Flags set"
-    else:
-        if flags & SYN:
-            print "SYN Flag set"
-        if flags & ACK:
-            print "ACK Flag set"
-    if flags & FIN:
-        print "FIN Flag set"
+    if debug:
+        print ("IP Source:          " + str(packet.getlayer(IP).src) + ":" + str(packet.getlayer(TCP).sport))
+        print ("IP Destin:          " + str(packet.getlayer(IP).dst) + ":" + str(packet.getlayer(TCP).dport))
+        print("TCP Payload Length:  " + str(payload_length))
+        print("Flags:               " + str(flags))
+        print("TCP ACK#:            " + str(in_ack))
+        print("TCP SEQ#:            " + str(in_seq))
+        if flags & PSH:
+            print "PSH Flag set"
+        if (flags & (SYN ^ ACK)) == 18:
+            print "SYN/ACK --> both Flags set"
+        else:
+            if flags & SYN:
+                print "SYN Flag set"
+            if flags & ACK:
+                print "ACK Flag set"
+        if flags & FIN:
+            print "FIN Flag set"
 
-
+    #### ACK# = SEQ# + Payload Länge + 1
+    #### SEQ# = ACK#
     ack_nr = in_seq + payload_length + 1
     seq_nr = in_ack
+
     if payload_length > 0 or (flags & (SYN ^ ACK)) == 18:
         send_flags = 'A'
-        print ("ACK wird geschickt mit ACK#=" + str(ack_nr) + " und SEQ#=" + str(seq_nr))
+        if debug:
+            print ("ACK wird geschickt mit ACK#=" + str(ack_nr) + " und SEQ#=" + str(seq_nr))
         send_tcp(dst_port, seq_nr, ack_nr, send_flags)
         if (flags & (SYN ^ ACK)) == 18:
             syn_ack_received_send_http_req(dst_port, seq_nr, ack_nr)
     if flags & FIN:
         send_flags = 'F''A'
-        print ("FIN/ACK wird geschickt mit ACK#=" + str(ack_nr) + " und SEQ#=" + str(seq_nr))
+        if debug:
+            print ("FIN/ACK wird geschickt mit ACK#=" + str(ack_nr) + " und SEQ#=" + str(seq_nr))
         send_tcp(dst_port, seq_nr, ack_nr, send_flags)
-    print ("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\r\n\r\n\r\n")
+    if debug:
+        print ("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\r\n\r\n\r\n")
     return
 
 ### Thread Klasse initiieren fuer ACK
