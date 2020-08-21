6a189886ce8fb76fc87e8c3430c9dd42ed12e23f
Benjamin Fuhlbrügge <s176110@hft-leipzig.de>
2020-08-21 20:21:03 +0200

    cosmetics

--- inject.py
+++ inject.py
@@ -4,11 +4,13 @@ from scapy.all import PcapReader, wrpcap, Packet, NoPayload
 from scapy.all import *
 
 
+
+vtep="192.168.10.149"
+source_ip="172.20.10.254"
 vxlanport=4789     # RFC 7384 port 4789, Linux kernel default 8472
+vni = 1
 broadcastmac="ff:ff:ff:ff:ff:ff"
 randommac="00:51:52:01:02:03"
-vtep="192.168.10.149"
-attacker="172.20.10.254"
 
 # send vxlan packet
-send(IP(src=attacker,dst=vtep)/UDP(sport=vxlanport,dport=vxlanport)/VXLAN(vni=(1),flags="Instance")/Ether(dst=broadcastmac,src=randommac))
+send(IP(src=source_ip,dst=vtep)/UDP(sport=vxlanport,dport=vxlanport)/VXLAN(vni=vni,flags="Instance")/Ether(dst=broadcastmac,src=randommac))
